6d85e2ab689355e55556a7566d6da771a5c906b3
Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-20 21:02:32 -0500

    code cuda

--- CUDA/Script_ejecutar_todo.sh
+++ CUDA/Script_ejecutar_todo.sh
@@ -1,8 +1,14 @@
 echo "Multiplicación de matrices en OMP" > resultados.txt
 
 
-for i in  8 16 32 64 128 256 512 1024  #Imagenes
+for n in  8 16 32 64 128 256 512 1024  #Imagenes
 	do
-		echo "Ejecucion con $i hilos." >> resultados.txt
-					(time ./omp "$i") &>> resultados.txt
+	for t in 16 64 256 1024 4096
+		do
+		for b in 1 8 16 32
+			do
+					echo "Ejecucion matrix $n x $n con $t hilos y $b bloques." >> resultados.txt
+							(!time ./NxN $n $b $t) &>> resultados.txt
+			done
+		done
   done
