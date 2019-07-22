5b0cb4c298e05819652b63a847ecc89e21f74cb6
Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-21 19:44:40 -0500

    script cuda

--- CUDA/Script_ejecutar_todo.sh
+++ CUDA/Script_ejecutar_todo.sh
@@ -5,7 +5,7 @@ for n in  8 16 32 64 128 256 512 1024  #Matrix
 	do
 	for t in 16 64 256 1024 4096 #Hilos
 		do
-		for b in 8 16 32 64#Blocks
+		for b in 8 16 32 64 #Blocks
 			do
 					echo "Ejecucion matrix $n x $n con $t hilos y $b bloques." >> resultados.txt
 							(time ./cuda "$n" "$t" "$b") &>> resultados.txt
