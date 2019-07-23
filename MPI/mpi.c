b393edf294e9d88184cf8de0e8fb5c1b1c851ae8
Juan Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-23 01:01:18 -0500

    s

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -1,59 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <float.h>
-#include <mpi.h>
+#include "mpi.h"
 
-int n, p;
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    int myn, myrank;
-    double *a, *b, *c, *allB, start, sum, *allC, sumdiag;
-    int i, j, k;
-    n = atoi(argv[1]);
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&p);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
-    myn = n/p;
-    a = malloc(myn*n*sizeof(double));
-    b = malloc(myn*n*sizeof(double));
-    c = malloc(myn*n*sizeof(double));
-    allB = malloc(n*n*sizeof(double));
-    for(i=0; i<myn*n; i++)
-    {
-        a[i] = 1.;
-        b[i] = 2.;
+	int N = atoi(argv[1]);
+  MPI_Status status;
+  int me,p;
+  int i,j,k;
+  int a[N][N],b[N][N],c[N][N];
+	if (taskid == 0) {
+    for (i=0; i<N; i++) {
+      for (j=0; j<N; j++) {
+        a[i][j]= (rand()%10);
+        b[i][j]= (rand()%10);
+      }
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(myrank==0)
-        start = MPI_Wtime();
 
-    for(i=0; i<p; i++)
-        MPI_Gather(b, myn*n, MPI_DOUBLE, allB, myn*n, MPI_DOUBLE,i, MPI_COMM_WORLD);
-    for(i=0; i<myn; i++)
-        for(j=0; j<n; j++)
-        {
-            sum = 0.;
-            for(k=0; k<n; k++)
-                sum += a[i*n+k]*allB[k*n+j];
-            c[i*n+j] = sum;
-        }
-    free(allB);
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(myrank==0)
-        printf("It took %f seconds to multiply 2 %dx%d matrices.\n",MPI_Wtime()-start, n, n);
-    if(myrank==0)
-        allC = malloc(n*n*sizeof(double));
-    MPI_Gather(c, myn*n, MPI_DOUBLE, allC, myn*n, MPI_DOUBLE,0, MPI_COMM_WORLD);
-    if(myrank==0)
-    {
-        for(i=0, sumdiag=0.; i<n; i++)
-            sumdiag += allC[i*n+i];
-        printf("The trace of the resulting matrix is %f\n", sumdiag);
-    }
-    if(myrank==0)
-        free(allC);
-    MPI_Finalize();
-    free(a);
-    free(b);
-    free(c);
-} 
+  /* Start up MPI */
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &me);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+  printf("me=%d, p=%d", me, p);
+
+  /* Data distribution */
+  if (me == 0) // master
+  {
+      // assume p = 2
+      for (i=1; i<p; i++)
+      {
+          printf("send to  %d with data from: %d and size:%d \n", i, (i)*N/p, N*N/p);
+          MPI_Send(&a[i * N / p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD);
+          MPI_Send(b, N * N, MPI_INT, i, 0, MPI_COMM_WORLD);
+      }
+  }
+  else
+  {
+      printf("Recv from %d with data from: %d and size:%d \n", 0, (me)*N/p, N*N/p);
+      MPI_Recv(&a[me * N / p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+      MPI_Recv(b, N * N, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+
+  }
+  /* Computation */
+  for(i=me * N / p; i<(me+1) * N/p; ++i)
+  {
+      for (j=0; j < N; j++)
+      {
+          c[i][j] = 0;
+          for (k=0; k<N; k++)
+          {
+              c[i][j] += a[i][k] * b[k][j];
+          }
+      }
+  }
+
+  /* Result gathering */
+  if (me != 0 )
+  {
+      MPI_Send(&c[(me) * N/p][0], N*N/p, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  }
+  else
+  {
+      for (i=1; i<p; i++)
+      {
+          MPI_Recv(&c[i * N/p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+      }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+
+  /* print the matrix */
+  if (me == 0)
+  {
+      for (i=0; i<N; i++) {
+          printf("\n\t| ");
+          for (j=0; j<N; j++)
+              printf("%2d ", c[i][j]);
+          printf("|");
+      }
+  }
+
+
+  /* Quit */
+
+  MPI_Finalize();
+  return 0;
+}
