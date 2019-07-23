77b1258067f36e1198ad2847d5fbd1ef36d8199e
Juan Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-22 20:09:17 -0500

    sec

--- OMP/omp.c
+++ OMP/omp.c
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
 	}
 
 	/* realizar la multiplicación en paralelo */
-
+  #pragma omp parallel
 	{
 		int i, j, k, suma = 0;
 
@@ -30,11 +30,9 @@ int main(int argc, char const *argv[])
 
 		for(i = 0; i < N; i++)
 		{
-			#pragma omp parallel for
 			for(j = 0; j < N; j++)
 			{
 				c[i * N + j] = 0;
-				#pragma omp parallel for
 				for(k = 0; k < N; k++)
 				{
 					c[i * N + j] += a[i * N + k] * b[k * N + j];
