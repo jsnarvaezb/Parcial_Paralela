32729ff0f6fa7af17aa6dd76007f143f7d60593b
Juan Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-23 01:18:12 -0500

    code mpi

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -5,7 +5,7 @@ MPI_Status status;
 
 main(int argc, char **argv)
 {
-  int numtasks,taskid,numworkers,source,dest,rows,offset,i,j,k;
+  int numtasks,taskid,numworkers,source,dest,col,offset,i,j,k;
   int N = atoi(argv[1]);
   struct timeval start, stop;
   double a[N][N],b[N][N],c[N][N];
@@ -28,24 +28,24 @@ main(int argc, char **argv)
       }
     }
 
-    rows = N/numworkers;
+    col = N/numworkers;
     offset = 0;
 
     for (dest=1; dest<=numworkers; dest++)
     {
       MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-      MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-      MPI_Send(&a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
+      MPI_Send(&col, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&a[offset][0], col*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
       MPI_Send(&b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-      offset = offset + rows;
+      offset = offset + col;
     }
 
     for (i=1; i<=numworkers; i++)
     {
       source = i;
       MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-      MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-      MPI_Recv(&c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&col, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&c[offset][0], col*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
     }
 
    /*printf("Matriz A:\n");
@@ -73,12 +73,12 @@ main(int argc, char **argv)
   if (taskid > 0) {
     source = 0;
     MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&col, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&a, col*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
     MPI_Recv(&b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
 
     for (k=0; k<N; k++)
-      for (i=0; i<rows; i++) {
+      for (i=0; i<col; i++) {
         c[i][k] = 0.0;
         for (j=0; j<N; j++)
           c[i][k] = c[i][k] + a[i][j] * b[j][k];
@@ -86,8 +86,8 @@ main(int argc, char **argv)
 
 
     MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-    MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-    MPI_Send(&c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&col, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&c, col*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
   }
 
   MPI_Finalize();
