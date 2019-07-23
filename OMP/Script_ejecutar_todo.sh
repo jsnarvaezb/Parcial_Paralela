d98af268990c1a83faf2ff553b4193972f2c3bbc
jsnarvaezb <jsnarvaezb@unal.edu.co>
2019-07-23 08:01:26 -0500

    Update Script_ejecutar_todo.sh

--- OMP/Script_ejecutar_todo.sh
+++ OMP/Script_ejecutar_todo.sh
@@ -2,8 +2,9 @@ echo "Multiplicación de matrices en OMP" > resultados.txt
 
 
 for i in  8 16 32 64 128 256 512 1024  #Imagenes
+  do
   for t in 1 2 4 8 16
-    do 
+   
 	do
 		echo "Ejecucion $i x $i con $t hilos." >> resultados.txt
 					(time ./omp "$i" "$t") &>> resultados.txt
