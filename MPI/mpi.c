394b44edb8d842161cc4a2b73b5ca81a848b6768
Juan Sebastian Narvaez <jsnarvaezb@unal.edu.co>
2019-07-23 01:03:52 -0500

    s

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
   int me,p;
   int i,j,k;
   int a[N][N],b[N][N],c[N][N];
+
+	int numtasks,taskid;
+
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
